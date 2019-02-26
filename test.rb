e6714c2f39a4a83879e3d615b095a6708cb45a83
Melissa Manousos <mmanousos@gmail.com>
2019-02-26 11:45:19 -0800

    Removes test.rb

--- test.rb
+++
@@ -1,4 +0,0 @@
-flights = [
-  { airline: 'American', date: '12-22-19', time: '09:30', destination: 'Atlanta'},
-  { airline: 'Southwest', date: '03-15-19', time: '22:40', destination: 'Boston'},
-]
